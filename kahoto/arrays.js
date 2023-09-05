4556f4dae2257c76a9cd8ebc4c1dfa066bd5cb9f
Luiza Marinho <luiza.calheirosm@gmail.com>
2023-09-05 20:51:17 -0300

    desfazendo a zebra

--- kahoto/arrays.js
+++
@@ -1,7 +0,0 @@
-let lista = 'batata, queijo, pizza, ketchup, pão';
-
-let lista_2 = lista.split(',');
-
-for (let indice = 0; indice < lista_2.length; indice += 1) {
-  console.log(lista[indice]);
-}
